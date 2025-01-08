4f1282274fd1f0eac2df9b353332a264e85a5162
safrizal <rzmobiledev@gmail.com>
2025-01-08 19:48:51 +0700

    wrap ProductList inside suspense page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts} from "@/lib/actions/product.actions";
 import {Product} from "@/types";
+import Loading from "@/app/loading";
 
 const Homepage = async() => {
     const latestProducts = await getLatestProducts<Product>();
     return (
         <>
-            <ProductList data={latestProducts} title="Newest Arrivals"/>
+            <Suspense fallback={<Loading />}>
+                <ProductList data={latestProducts} title="Newest Arrivals"/>
+            </Suspense>
         </>
     );
 };
--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,11 @@
 import {z} from 'zod'
 import {insertProductSchema} from "@/lib/validators";
 
-type ProductData = z.infer<typeof insertProductSchema> & {
+export type Product = z.infer<typeof insertProductSchema> & {
     id: string
-    rating: string
-    createdAt: Date
-}
-
-export type Product = Omit<ProductData, 'price' | 'rating'> & {
     price: number
     rating: string
+    createdAt: Date
 }
 
 export const dataProducts = <T extends Product, K extends keyof T>(products: T[K]): T[K] => products
