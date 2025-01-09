045593e487e2191d10594c6d0297896039a140b4
safrizal <rzmobiledev@gmail.com>
2025-01-09 08:16:07 +0700

    update package.json (add npx before prisma generate in postinstall)

--- db/prisma.ts
+++ db/prisma.ts
@@ -14,19 +14,4 @@ const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
-export const prisma = new PrismaClient({ adapter }).$extends({
-  result: {
-    product: {
-      price: {
-        compute(product) {
-          return product.price.toString();
-        },
-      },
-      rating: {
-        compute(product) {
-          return product.rating.toString();
-        },
-      },
-    },
-  },
-});
+export const prisma = new PrismaClient({ adapter })
