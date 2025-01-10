7bf28f58c6c9a43482baeb392bbaa34eaaabaeaf
safrizal <rzmobiledev@gmail.com>
2025-01-10 11:01:14 +0700

    removing error for unused usericon in component header menu page

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,7 +1,7 @@
 import ModeToggle from "@/components/shared/header/mode-toggle";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
-import {EllipsisVertical, ShoppingCart, UserIcon} from "lucide-react";
+import {EllipsisVertical, ShoppingCart} from "lucide-react";
 import {Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger} from "@/components/ui/sheet";
 import UserButton from "@/components/shared/header/user-button";
 
